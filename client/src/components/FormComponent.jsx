0a7de830b65977ac4d654b910ad33e53ccceee16
TallapallyAneesh <ta22mmb0a45@student.nitw.ac.in>
2025-01-05 18:31:12 +0530

    add form element

---
+++ client/src/components/FormComponent.jsx
@@ -0,0 +1,12 @@
+function FormComponent() {
+  return (
+    <div className="h-16 w-60">
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username " placeholder="Username"></input>
+      </form>
+    </div>
+  );
+}
+
+export default FormComponent;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,12 @@
 function SignUp() {
-  return <div>signup</div>
+  return (
+    <div>
+      <h1 className="font-bold text-4xl text-center my-7">Sign Up</h1>
+      <form>
+        <input type="text"></input>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
