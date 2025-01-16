3fa518c272339931cc5cd4da46a276bcf4f2acbd
TallapallyAneesh <ta22mmb0a45@student.nitw.ac.in>
2025-01-16 17:33:15 +0530

    create user model

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,11 +1,28 @@
 import mongoose from "mongoose";
 
-const UserSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 3,
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+    },
   },
-});
+  { timestamps: true }
+);
+export default mongoose.model("User", UserSchema);
