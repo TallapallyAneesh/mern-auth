b540dfc0414ef7bb1fdb465883e42df7238cba82
TallapallyAneesh <ta22mmb0a45@student.nitw.ac.in>
2025-01-16 17:47:26 +0530

    add test api route

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "../database/database.js";
 import "dotenv/config";
+import userRoutes from "../routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,3 +16,5 @@ connectDB()
     console.Error(`Error: ${error.message}`);
     console.log("Server is not running");
   });
+
+app.use("/api/user", userRoutes); //this is just a test route to check if the server is running
